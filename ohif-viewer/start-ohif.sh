d3e696d4cd81194afd4935ffc9dd3750a2cf080b
Martin van Kuik <mtjvankuik@gmail.com>
2023-10-06 15:33:24 +0200

    Build and deploy local ohif-image due to missing public ohif image for arm64 architecture

--- ohif-viewer/start-ohif.sh
+++ ohif-viewer/start-ohif.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 docker rm -f ohif-viewer-container
+docker image rm ohif-viewer-image:latest
+
+docker build /projects/Viewers -t ohif-viewer-image
+
 docker pull ohif/viewer
-docker run -d -p 3000:80/tcp --name ohif-viewer-container --platform linux/arm64/v8 ohif/viewer
+docker run -d -p 3000:80/tcp --name ohif-viewer-container --platform linux/arm64/v8 ohif-viewer-image
