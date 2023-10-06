fc9f676a82ab948fd89da7b020b8c5d582d9dd6d
Martin van Kuik <mtjvankuik@gmail.com>
2023-10-06 15:39:15 +0200

    start-ohif pull from public repo removed and removed --platform flag in docker run

--- ohif-viewer/start-ohif.sh
+++ ohif-viewer/start-ohif.sh
@@ -5,5 +5,4 @@ docker image rm ohif-viewer-image:latest
 
 docker build /projects/Viewers -t ohif-viewer-image
 
-docker pull ohif/viewer
-docker run -d -p 3000:80/tcp --name ohif-viewer-container --platform linux/arm64/v8 ohif-viewer-image
+docker run -d -p 3000:80/tcp --name ohif-viewer-container ohif-viewer-image
