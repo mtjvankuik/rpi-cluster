df2ba3d1f41537542096843d4b91ef842ddb63b1
Martin van Kuik <mtjvankuik@gmail.com>
2023-10-08 13:40:43 +0200

    Docker ohif start-ohif.sh removed ohif image removal after startup

--- ohif-viewer/start-ohif.sh
+++ ohif-viewer/start-ohif.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 docker container rm --force ohif-viewer
-docker image rm --force mtjvankuik/ohif:v3.7.0-beta.102
-
 docker image pull mtjvankuik/ohif:v3.7.0-beta.102
 docker run --detach --publish 3000:80/tcp --name ohif-viewer mtjvankuik/ohif:v3.7.0-beta.102
