f915f1a92edf839c9f23869662531571ccdffc54
Martin van Kuik <mtjvankuik@gmail.com>
2023-11-25 20:39:53 +0100

    minecraft backup script removes old backups older than 31 days

--- minecraft/backup-minecraft-data-packaged.sh
+++ minecraft/backup-minecraft-data-packaged.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo "Nightly Backup Started: $(date)" >> /tmp/mybackup.log
+
 # make backup folder on synology share if folder not exists
 if [ ! -d /mnt/synology/volume1/backup/minecraft_backup/ ]; then
 mkdir -p /mnt/synology/volume1/backup/minecraft_backup/;
@@ -8,3 +10,7 @@ fi
 # make tar.gz package and output to network share
 tar -czvf "/mnt/synology/volume1/backup/minecraft_backup/$minecraft_data_$(date +%Y%m%d).tar.gz" /projects/docker-minecraft-server/minecraft_data
 
+# remove old backup - rolling time window of 31 days
+rm -f "/mnt/synology/volume1/backup/minecraft_backup/$minecraft_data_$(date -d "$today - 31 days" +%Y%m%d).tar.gz"
+
+echo "Nightly Backup Finished: $(date)" >> /tmp/mybackup.log
