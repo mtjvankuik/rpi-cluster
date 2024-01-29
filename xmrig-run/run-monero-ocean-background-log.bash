32b17f0c32df53dce5f4ef34726f33bb50bfb0a9
Martin <mtjvankuik@gmail.com>
2024-01-29 20:14:16 +0000

    run-monero-ocean mining pool bash script

---
+++ xmrig-run/run-monero-ocean-background-log.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+./xmrig \
+ --url=gulf.moneroocean.stream:10128 \
+ --user=49Zi1HbHAWh9T8fBS6MWRbEJ2A3wYENToLKCqNUrFPzg2LizeFJR1imJxh4fjtYxwbHSgj6jVgrXoAiZMmmQ4NPL791m4yk \
+ --coin=monero \
+ --log-file=/var/log/xmrlog.log \
+ --verbose \
+ --background
---
+++ xmrig-run/run-monero-ocean-background.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+./xmrig -o gulf.moneroocean.stream:10128 -u 49Zi1HbHAWh9T8fBS6MWRbEJ2A3wYENToLKCqNUrFPzg2LizeFJR1imJxh4fjtYxwbHSgj6jVgrXoAiZMmmQ4NPL791m4yk --coin=monero --background
+
